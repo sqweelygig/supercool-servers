0330eabf46aedc3f970e08a36ba8540fcfa58e0b
Andrew Lucas <sqweelygig@googlemail.com>
2022-08-17 11:44:05 +0100

    Remove a todo that related to Google Chrome's zoom finesse

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 // TODO Sort out github sites deployment
 // TODO Style for desktop resolutions
-// TODO Investigate the 1 pixel gap
 
 import { createApp } from "vue";
 import SuperCoolServers from "./SuperCoolServers.vue";
